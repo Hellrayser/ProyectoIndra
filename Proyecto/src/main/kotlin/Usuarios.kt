dc7bba1a5f574d2804b2bfe92d116293471d764a
Ian <117636542+Hellrayser@users.noreply.github.com>
2025-02-19 14:33:54 +0100

    Metodos Usuarios

--- Proyecto/src/main/kotlin/Usuarios.kt
+++ Proyecto/src/main/kotlin/Usuarios.kt
@@ -8,4 +8,32 @@ class Usuarios {
         evento.inscribirUsuario(this)
         eventosApuntados.add(evento)
     }
+
+    fun cancelarInscripcion(evento:Eventos){
+        var i = 0
+        var inscrito = false
+        while (i<eventosApuntados.size && !inscrito){
+            if (eventosApuntados[i]==evento){
+                eventosApuntados.removeAt(i)
+                inscrito = true
+            }
+            else {
+                i++
+            }
+        }
+        if (inscrito){
+            println("Se ha cancelado la inscripción de ${this.nombre} al evento ${evento.nombre}")
+        } else {
+            println("${this.nombre} no estaba inscrito a ese evento")
+        }
+    }
+
+    fun monstrarEventosInscritos(){
+        println("Eventos en los que ${this.nombre} esta inscrito")
+        var i = 0
+        while (i<eventosApuntados.size){
+            println(eventosApuntados[i].nombre)
+            i++
+        }
+    }
 }
